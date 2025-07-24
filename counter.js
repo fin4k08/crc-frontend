1cc895ba3a6e80d2a895298153090c94e51ba440
Andrew Findlay <fin4k08@live.co.uk>
2025-07-24 08:48:57 +0100

    Initial commit: Frontend static site

---
+++ counter.js
@@ -0,0 +1,9 @@
+fetch("https://crc-func-dev.azurewebsites.net/api/GetVisitorCount")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("visitor-count").innerText = data.count;
+  })
+  .catch(err => {
+    console.error("Error fetching visitor count:", err);
+    document.getElementById("visitor-count").innerText = "N/A";
+  });
